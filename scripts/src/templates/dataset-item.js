4443476ea51a5c4a802e3e12d60aca6b31c9ab77
amaliebarras <amaliebarras@gmail.com>
2016-07-25 15:55:41 -0500

    fixing dataset tiles and adding construction banner

--- scripts/src/templates/dataset-item.js
+++ scripts/src/templates/dataset-item.js
@@ -17,21 +17,13 @@ return `
                 <div class="row">
                     <div class="col-xs-5">
                         <div class="score">
-                            <h1>${score || 'N/A'}%</h1>
-                        </div>
-                        <div class="ratings">
-                            <p class = "whitetext"><span>${data.ratings || '0'}</span> ratings</p>
-                        </div>
-                        <div class="projects">
-                            <p class = "whitetext"><span>3</span> projects</p>
+                            <h1>${data.difficulty || 'N/A'}</h1>
                         </div>
                     </div>
                     <div class="col-xs-7">
                         <div class="description">
                             <p>${data.notes || ''}</p>
                         </div>
-                        <div class="format">
-                            <h2><i class="fa fa-file-pdf-o" aria-hidden="true"></i><i class="fa fa-file-excel-o" aria-hidden="true"></i></h2></div>
                     </div>
                 </div>
             </div>
